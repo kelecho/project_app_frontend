fbf7e9bb67e73ac349248b1f15a929d370ddb3d6
Arnix Minadeo <arnix676@gmail.com>
2023-05-01 15:39:13 -0400

    Botones viejos de editar y eliminar quitados

--- src/pages/Proyecto.jsx
+++ src/pages/Proyecto.jsx
@@ -41,17 +41,6 @@ const Proyecto = () => {
     <>
         <div className="flex justify-between">
         <h1 className="font-black text-4xl">{proyecto.nombre}</h1>
-
-        <div className='flex items-center gap-2 text-gray-400 hover:text-black'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-          </svg>
-
-          <Link
-            className='uppercase font-bold'
-            to={`/proyectos/editar/${id}`}
-          >Editar</Link>
-        </div>
       </div>
       <div className="flex justify-between mt-20 items-center">
       <button
@@ -66,7 +55,7 @@ const Proyecto = () => {
       </button>
         <div className='flex items-center gap-4'>
           <img src={proyecto.icono ? proyecto.icono : ''} alt='icono' className='flex w-10 h-full rounded-md bg-center bg-no-repeat' />
-          <h1 className="font-black text-4xl">{nombre}</h1>
+          <h1 className="font-black text-4xl">{proyecto.nombre}</h1>
         </div>
         <div className="gap-2">
         <div className='flex items-center gap-2 text-gray-400 hover:text-black'>
@@ -93,7 +82,7 @@ const Proyecto = () => {
 
       <p className="font-bold text-xl mt-10">Tareas del Proyecto</p>
 
-      <Navbar setFiltro={setFiltro}/>
+      {/* <Navbar setFiltro={setFiltro}/> */}
 
       { msg && <Alerta alerta={alerta} />}
 
